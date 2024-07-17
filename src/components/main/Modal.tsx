52322daf618dc385a299506bf79473ceb6422353
JongYun Jeong <95991290+BellYun@users.noreply.github.com>
2024-07-17 15:36:53 +0900

    feat : 설정 모달 구현

--- src/components/main/Modal.tsx
+++ src/components/main/Modal.tsx
@@ -11,10 +11,8 @@ const Modal = ({ isOpen, onClose, children }) => {
   };
   return ReactDOM.createPortal(
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"  onClick={handleOutsideClick}>
-      <div className="bg-white p-6 rounded shadow-lg" onClick={(e) => e.stopPropagation()}>
-        <button className="absolute top-2 right-2" onClick={onClose}>
-          Close
-        </button>
+      <div className="bg-white p-6 rounded-2xl shadow-lg" onClick={(e) => e.stopPropagation()}>
+
         {children}
       </div>
     </div>,
--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Carousel from '../../components/main/Carousel/Carousel'
 import Modal from '../../components/main/Modal';
 
@@ -27,47 +27,64 @@ const MainPage = () =>{
       "disease": [], // 빈 배열 유지
       // 프론트 데이터에 따라서 요소가 변경될 수 있음
     },
-    {
-      "id": 2,
-      "petName": "꼬미",
-      "imgUrl": "...",
-      "birthday": "2022-06-22", // 날짜 형식 유지
-      "gender": "male",
-      "species": "dog", // 예시로 "dog" 추가
-      "weight": 17,
-      "disease": [], // 빈 배열 유지
-      // 프론트 데이터에 따라서 요소가 변경될 수 있음
-    },
-    {
-      "id": 2,
-      "petName": "꼬미",
-      "imgUrl": "...",
-      "birthday": "2022-06-22", // 날짜 형식 유지
-      "gender": "male",
-      "species": "dog", // 예시로 "dog" 추가
-      "weight": 17,
-      "disease": [], // 빈 배열 유지
-      // 프론트 데이터에 따라서 요소가 변경될 수 있음
-    },
-
-    //모달 관리
-    //설정 input관리
   ];
 
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [chooseDelete, setChooseDelete] = useState(false);
+  //TODO: 설정 input은 상태 관리 라이브러리 활용
+  //TODO: 수정 삭제 선택 기능
+
   const handleModalOpen = () => {
     setIsModalOpen(true);
   };
+  const handleDelete = () => {
+    setChooseDelete(true);
+  }
+  //TOOD: API연동
 
+  useEffect(()=>{
+    if(!isModalOpen){
+      setChooseDelete(false);
+    }
+  },[isModalOpen])//초기화
+
+  
   return(
     <section className='flex flex-row justify-center bg-[#FEFEFE]'>
       <Carousel slides={data} HandleModal={handleModalOpen} />
-      
-      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
-        <h2 className="text-2xl mb-4">Modal Content</h2>
-        <p>Here is some content inside the modal!</p>
+      <Modal isOpen={isModalOpen}  onClose={() => setIsModalOpen(false)} >
+        <div className='flex flex-col justify-center items-center'>
+          {
+            chooseDelete ?
+            <>
+              <div>
+                삭제하시겠습니까?
+              </div>
+              <div>
+                예
+              </div>
+              <div onClick={()=>setChooseDelete(false)} >
+                아니오
+              </div>
+            </>
+            :
+            <>
+              <h2 className="item text-xl mb-4">설정</h2>
+              <div className='flex flex-row'>
+                <div>
+                  수정
+                </div>
+                /
+                <div onClick={()=>handleDelete()}>
+                  삭제
+                </div>
+              </div>
+            </>
+          }
+
+        </div>
+        {/* TODO: 삭제하시겠습니까? 추가 후 삭제 구현 */}
       </Modal>
-      {/* <PetCardCarousel/> */}
     </section>
   )
 }
