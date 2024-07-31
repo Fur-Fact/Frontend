90336a82815aa66c4fca667817e2fe598662a8c6
JongYun Jeong <95991290+BellYun@users.noreply.github.com>
2024-07-31 20:46:44 +0900

    hotfix : baseURL 오류 수정

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3000/api/v1/';
-// const BASE_URL = 'https://furfact.site/api/v1/';
+// const BASE_URL = 'http://localhost:3000/api/v1/';
+const BASE_URL = 'https://furfact.site/api/v1/';
 
 export const baseInstance = axios.create({
   withCredentials: true,
