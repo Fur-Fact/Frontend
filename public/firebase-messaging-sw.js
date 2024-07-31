35c0a8e21fc1c0f52be6a03b3843f0a7f5e3476e
JongYun Jeong <95991290+BellYun@users.noreply.github.com>
2024-07-31 16:29:56 +0900

    hotfix : 알림 처리 임시 테스트

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -5,3 +5,20 @@ self.addEventListener("install", function (e) {
 self.addEventListener("activate", function (e) {
   console.log("fcm sw activate..");
 });
+
+self.addEventListener("push", function (e) {
+  if (!e.data.json()) return;
+
+  const resultData = e.data.json().notification;
+  const notificationTitle = resultData.title;
+
+  const notificationOptions = {
+    body: resultData.body,
+  };
+
+  console.log(resultData.title, {
+    body: resultData.body,
+  });
+
+  e.waitUntil(self.registration.showNotification(notificationTitle, notificationOptions));
+});
